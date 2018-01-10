d28c3e99ffaebcbe716590f394f1b8b3be19f487
DariaKaidalova <dariakaidalova@gmail.com>
2018-01-10 15:42:00 +0200

    tests

--- angular-2-cli/angular-app/src/app/app.component.spec.ts
+++ angular-2-cli/angular-app/src/app/app.component.spec.ts
@@ -1,32 +1,29 @@
 import { TestBed, async } from '@angular/core/testing';
+import { Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// import { SpinnerComponent } from './spinner/spinner.component';
+import { MainComponent } from './main/main.component';
 
-describe('AppComponent', () => {
+describe('Component: ', () => {
+
+  let component: MainComponent;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MainComponent
       ],
     }).compileComponents();
+
+    const spinnerComponent = TestBed.createComponent(MainComponent);
+    component = spinnerComponent.componentInstance;
   }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+  it('MainComponent should be difined in the AppComponent', () => {
+    expect(component).toBeDefined();
+  });
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
 });
--- angular-2-cli/angular-app/src/app/pipe/сrop-text-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { СropTextPipe } from './сrop-text-pipe.pipe';
-
-describe('СropTextPipe', () => {
-  it('create an instance', () => {
-    const pipe = new СropTextPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
