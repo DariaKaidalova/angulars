f49d3b663f2bf706637690511caf576d65f81c27
DariaKaidalova <dariakaidalova@gmail.com>
2018-01-12 13:54:02 +0200

    tests

--- angular-2-cli/angular-app/src/app/app.component.spec.ts
+++ angular-2-cli/angular-app/src/app/app.component.spec.ts
@@ -1,55 +1,36 @@
 import { TestBed, async } from '@angular/core/testing';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SpinnerComponent } from './spinner/spinner.component';
 import { MainComponent } from './main/main.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { LanguagesService } from './service/languages.service';
 
 describe('AppComponent: ', () => {
 
-  let spinner: SpinnerComponent, main: MainComponent;
-  var fixture, app, spinnerComponent, mainComponent;
+  let app: AppComponent;
+
+  let mockLang = {
+    determineLanguage() { return true }
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent,
-        SpinnerComponent,
-        MainComponent
+        AppComponent
       ],
-      imports: [Component]
-      
+      providers: [{provide: LanguagesService, useValue: mockLang }],
+      schemas: [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
-    fixture = TestBed.createComponent(AppComponent);
-    app = fixture.debugElement.componentInstance;
 
-    spinnerComponent = TestBed.createComponent(SpinnerComponent);
-    spinner = spinnerComponent.componentInstance;
-
-    mainComponent = TestBed.createComponent(MainComponent);
-    main = mainComponent.componentInstance;
+    const appComponent = TestBed.createComponent(AppComponent);
+    app = appComponent.componentInstance;
   }));
 
-  it('Should create the App', async(() => {
-    expect(app).toBeTruthy();
+
+  it('Should work', async(() => {
+    expect(app).toBeDefined();
   }));
-
-  // it('Should contain SpinnerComponent', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector(spinner)).not.toBe(null);
-  // }));
-
-  // it('Should contain MainComponent', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector(main)).not.toBe(null);
-  // }));
-
-  // it('MainComponent should be difined in the AppComponent', () => {
-  //   expect(main).not.toBeDefined();
-  // });
 
 });
